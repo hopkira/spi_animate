52068ba0d0937962d385eaaabdaa9faad4fcaa85
Richard Hopkins <hopkira@googlemail.com>
2023-11-30 23:16:38 +0000

    Move to ram drive

---
+++ anim.py
@@ -0,0 +1,8 @@
+import shutil
+source_prefix = "/home/hopkira/spi_animate/"
+target_prefix = "/var/tmp/"
+for x in range(90):
+    source = source_prefix + "frame_{0:02d}_delay-0.06s.gif".format(x)
+    target = target_prefix + "01_{0:02d}.gif".format(x)
+    shutil.copyfile(source, target)
+    print(source,"->",target)
