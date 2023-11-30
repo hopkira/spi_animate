3a4b8f2485773d14bac2927191f157012394b238
Richard Hopkins <hopkira@googlemail.com>
2023-11-30 23:40:20 +0000

    Display file

--- anim.py
+++ anim.py
@@ -1,4 +1,10 @@
 import shutil
+import logging
+sys.path.append("..")
+from lib import LCD_1inch28
+from PIL import Image,ImageDraw
+
+# load images into Pi memory
 source_prefix = "/home/hopkira/spi_animate/images/"
 target_prefix = "/var/tmp/"
 for x in range(90):
@@ -6,3 +12,19 @@ for x in range(90):
     target = target_prefix + "01_{0:02d}.gif".format(x)
     shutil.copyfile(source, target)
     print(source,"->",target)
+
+try:
+    disp = LCD_1inch28.LCD_1inch28()
+    disp.Init()
+    disp.clear()
+    while True:
+        for x in range(90):
+            image_file = target_prefix + "01_{0:02d}.gif".format(x)
+            image = Image.open(image_file)      
+            disp.ShowImage(image)
+except IOError as e:
+    logging.info(e)    
+except KeyboardInterrupt:
+    disp.module_exit()
+    logging.info("quit:")
+    exit()
