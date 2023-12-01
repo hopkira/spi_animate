1f9c3b937f0cfe4d2e90014a1a18ebeb7670b914
Richard Hopkins <hopkira@googlemail.com>
2023-12-01 18:21:45 +0000

    Move to jpg

--- anim.py
+++ anim.py
@@ -21,8 +21,8 @@ device = 0
 source_prefix = "/home/hopkira/spi_animate/images/"
 target_prefix = "/var/tmp/"
 for x in range(90):
-    source = source_prefix + "frame_{0:02d}_delay-0.06s.gif".format(x)
-    target = target_prefix + "01_{0:02d}.gif".format(x)
+    source = source_prefix + "frame_{0:02d}_delay-0.06s.jpg".format(x)
+    target = target_prefix + "01_{0:02d}.jpg".format(x)
     shutil.copyfile(source, target)
     print(source,"->",target)
 
