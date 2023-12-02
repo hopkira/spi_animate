47ae3203f8d7a4aab7c97f5b7fcd85c3ca43f7de
Richard Hopkins <hopkira@googlemail.com>
2023-12-02 10:55:33 +0000

    Update anim.py

--- anim.py
+++ anim.py
@@ -15,14 +15,16 @@ DC = 25
 BL = 23
 bus = 0 
 device = 0
+colours = ["green","lime","orange","yellow","red",]
 
 # load images into Pi memory
-source_prefix = "/home/hopkira/spi_animate/images/"
-target_prefix = "/var/tmp/"
-for x in tqdm(range(90)):
-    source = source_prefix + "frame_{0:02d}_delay-0.06s.jpg".format(x)
-    target = target_prefix + "01_{0:02d}.jpg".format(x)
-    shutil.copyfile(source, target)
+for num, colour in iter(colours):
+    source_prefix = "/home/hopkira/spi_animate/images/" + colour
+    target_prefix = "/var/tmp/"
+    for x in tqdm(range(90)):
+        source = source_prefix + "frame_{0:02d}_delay-0.06s.jpg".format(x)
+        target = target_prefix + str(num) + "_{0:02d}.jpg".format(x)
+        shutil.copyfile(source, target)
 
 try:
     #disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl>
@@ -31,7 +33,7 @@ try:
     disp.clear()
     while True:
         for x in range(90):
-            image_file = target_prefix + "01_{0:02d}.jpg".format(x)
+            image_file = target_prefix + "4_{0:02d}.jpg".format(x)
             image = Image.open(image_file)
             disp.ShowImage(image)
             sys.stdout.write(next(spinner))   # write the next character
